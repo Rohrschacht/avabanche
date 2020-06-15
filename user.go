1ac49b1b0d2593c19af0e980d0d27bc9e481f492
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2020-06-15 16:20:11 +0200

    implement faster version of faucetusers

--- user.go
+++ user.go
@@ -80,33 +80,14 @@ func initUsers(usersfile string) {
 }
 
 func faucetUsers(usersfile string, amount uint, u user) {
-	users := loadUsers(usersfile)
-
-	for _, ur := range users {
-		if ur["address"] == "" {
-			fmt.Fprintf(os.Stderr, "There are uninitialized users in the usersfile!\n")
-			os.Exit(4)
+	min := func(a, b int) int {
+		if a > b {
+			return b
 		}
+		return a
 	}
 
-	balance, err := getBalance(u["address"])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not get balance for %s: %v\n", u["address"], err)
-		os.Exit(2)
-	}
-
-	if uint64(uint(len(users))*amount) > balance {
-		fmt.Fprintf(os.Stderr, "Address does not have enough funds: %d * %d > %d!\n", len(users), amount, balance)
-		os.Exit(2)
-	}
-
-	factory := transactionFactory{amount: amount, user: u}
-	durations := make(chan string, len(users))
-
-	for i, ur := range users {
-		factory.to = ur["address"]
-		tx := factory.newSensorTransaction(uint(i))
-
+	trySend := func(tx transaction) {
 		err := sendTx(tx)
 		if err != nil {
 			wentThrough := false
@@ -123,10 +104,86 @@ func faucetUsers(usersfile string, amount uint, u user) {
 				os.Exit(3)
 			}
 		}
+	}
 
-		tx.register(durations)
-		for !tx.isReady() {
-			time.Sleep(100 * time.Millisecond)
+	usersThatGet := loadUsers(usersfile)
+	usersThatGive := []user{}
+
+	for _, ur := range usersThatGet {
+		if ur["address"] == "" {
+			fmt.Fprintf(os.Stderr, "There are uninitialized users in the usersfile!\n")
+			os.Exit(4)
+		}
+	}
+
+	balance, err := getBalance(u["address"])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not get balance for %s: %v\n", u["address"], err)
+		os.Exit(2)
+	}
+
+	if uint64(amount) > balance {
+		fmt.Fprintf(os.Stderr, "Address does not have enough funds: %d > %d!\n", amount, balance)
+		os.Exit(2)
+	}
+
+	if amount < uint(len(usersThatGet)) {
+		fmt.Fprintf(os.Stderr, "Amount must be at least as high as number of users to faucet!: %d < %d\n", amount, len(usersThatGet))
+		os.Exit(2)
+	}
+
+	factory := transactionFactory{amount: amount, user: u}
+	durations := make(chan string, len(usersThatGet))
+
+	// Send first transaction to first user
+	factory.to = usersThatGet[0]["address"]
+	tx := factory.newSensorTransaction(0)
+	trySend(tx)
+	tx.register(durations)
+	for !tx.isReady() {
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	// Remove first user from usersThatGet and put it into usersThatGive
+	// append
+	usersThatGive = append(usersThatGive, usersThatGet[0])
+	// remove
+	copy(usersThatGet[0:], usersThatGet[1:])
+	usersThatGet[len(usersThatGet)-1] = user{}
+	usersThatGet = usersThatGet[:len(usersThatGet)-1]
+
+	for {
+		amount = amount / 2
+		factory.amount = amount
+		txList := []transaction{}
+
+		steps := min(len(usersThatGet), len(usersThatGive))
+		for i := 0; i < steps; i++ {
+			factory.user = usersThatGive[i]
+			factory.to = usersThatGet[0]["address"]
+
+			tx := factory.newSensorTransaction(0)
+			trySend(tx)
+			tx.register(durations)
+			txList = append(txList, tx)
+
+			// Remove first user from usersThatGet and put it into usersThatGive
+			// append
+			usersThatGive = append(usersThatGive, usersThatGet[0])
+			// remove
+			copy(usersThatGet[0:], usersThatGet[1:])
+			usersThatGet[len(usersThatGet)-1] = user{}
+			usersThatGet = usersThatGet[:len(usersThatGet)-1]
+		}
+
+		for _, tx := range txList {
+			for !tx.isReady() {
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+
+		if len(usersThatGet) == 0 {
+			break
 		}
 	}
 }
